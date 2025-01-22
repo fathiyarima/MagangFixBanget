58a49e276b6fc7cc9faf2df7ff46d1df7b74d878
FRDrops <rnispgjk@gmail.com>
2025-01-22 08:54:05 +0700

    koneksi

---
+++ config/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = "127.0.0.1";
+$username = "root";
+$password = "";
+$database = "sistem_ta";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$conn->close();
+?>
