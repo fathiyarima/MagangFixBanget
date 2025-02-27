f33be56949c607f97126524606f7b3d278a36244
FRDrops <112362185+FRDrops@users.noreply.github.com>
2025-02-27 12:02:47 +0700

    benerin

--- pages/dospem/daftarmahasiswa.php
+++ pages/dospem/daftarmahasiswa.php
@@ -260,9 +260,6 @@ try {
                                         </p>
                                         <div class="table-responsive">
                                             <?php
-                                            try {
-
-                                        $conn = new mysqli($servername, $username, $password, $dbname);
                                         if ($conn->connect_error) {
                                             die("Koneksi gagal: " . $conn->connect_error);
                                         }
